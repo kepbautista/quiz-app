b4e9d5d6ff465407e706097b378ec95fd7bfec7e
Kristine Bautista <kepbautista@gmail.com>
2025-05-11 00:45:28 +0800

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,56 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    root: true,
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react$', '^next', '^[a-z]', '^@'],
+            ['^~'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ['^.+\\.s?css$'],
+            ['^\\u0000'],
+          ],
+        },
+      ],
+    },
+    ignorePatterns: ['node_modules', '.next'],
+    overrides: [
+      {
+        files: ['tailwind.config.ts'],
+        rules: {
+          '@typescript-eslint/no-require-imports': 'off',
+        },
+      },
+    ],
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
